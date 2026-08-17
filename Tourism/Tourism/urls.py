7750fd803a789c929613f909e7c5390b6dbff5a5
kundana <kundhanasaipurru@gmail.com>
2026-08-17 19:38:01 +0530

    Retrive all tourist places

--- Tourism/Tourism/urls.py
+++ Tourism/Tourism/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
     path("about/",views.about,name="about"),
+    path("states/<uuid:state_id>/", views.get_state, name="get_state"),
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Tourism/Tourism_app/views.py
+++ Tourism/Tourism_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Homepage_video
-
+from django.http import JsonResponse
 # Create your views here.
 def home(request):
     return render(request,"home.html")
@@ -8,3 +8,8 @@ def home(request):
 def about(request):
     data = Homepage_video.objects.all()
     return render(request,"about.html",{"data":data})
+
+def tour_places(request):
+    places = TourPlace.objects.all()
+    return render(request,"my_topic.html",{"data":places})
+    
